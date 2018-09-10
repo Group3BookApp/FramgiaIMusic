9e3432712eead920fcd73c6e471d350918c361c0
Phamduc8797 <phamduc8797@gmail.com>
2018-09-10 10:24:31 +0700

    play-music

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   resources :categories, only: :index
-  resources :songs, only: :index
+  resources :songs, only: %i(index show)
   namespace :admin do
     resources :admin, only: :index
+    resources :songs
   end
+  resources :artists
 end
