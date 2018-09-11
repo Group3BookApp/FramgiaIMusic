f6979968ca18af332f51d9646758995c38e26c03
NguyenThu2808 <nguyenthu280896@gmail.com>
2018-09-11 14:15:41 +0700

    Manage users

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,7 +1,15 @@
 module Admin
   class AdminController < ApplicationController
+    before_action :authenticate_user!
+    before_action :verify_admin
     layout "admin/admin"
 
     def index; end
+
+    def verify_admin
+      return if current_user.admin?
+      flash[:danger] = t ".not_admin"
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,17 @@ Rails.application.routes.draw do
   root "static_pages#home"
   resources :categories, only: :index
   resources :songs, only: %i(index show)
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks:
+    "users/omniauth_callbacks"}
+  get "users/auth/google_oauth2", to: "sessions#create"
+  get "users/auth/failure", to: redirect("/")
+  get "signout", to: "devise/sessions#destroy", as: "signout"
+  resource :user, only: %i(show edit update)
   namespace :admin do
     resources :admin, only: :index
     resources :songs
+    resources :users, only: %i(index destroy)
   end
   resources :artists
 end
+
