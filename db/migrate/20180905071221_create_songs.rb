0b17965a3649d1d524ad0ff507fb3aa56fa88587
Phamduc8797 <phamduc8797@gmail.com>
2018-09-12 20:17:57 +0700

    manage-song

--- db/migrate/20180905071221_create_songs.rb
+++ db/migrate/20180905071221_create_songs.rb
@@ -3,11 +3,12 @@ class CreateSongs < ActiveRecord::Migration[5.2]
     create_table :songs do |t|
       t.string :name
       t.string :data
+      t.string :avatar
       t.integer :listening
       t.references :category, foreign_key: true
-      t.references :author, foreign_key: true
       t.references :album, foreign_key: true
       t.references :artist, foreign_key: true
+      t.references :author, foreign_key: true
 
       t.timestamps
     end
